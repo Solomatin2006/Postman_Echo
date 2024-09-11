09f6407eca7cab78b94954f59781d0281e3fbe6c
Aleksey Solomatin <2717070@list.ru>
2024-09-11 18:36:15 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PostmanEchoTest {
+
+
+    @Test
+    void shouldReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when().log().all()
+                .post("/post")
+// Проверки
+                .then().log().all()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("Hello"));
+
+    }
+}
